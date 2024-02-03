b560f043bcecde49756a20ad4e30674368c6f331
JethroNatividad <natividad.jet@gmail.com>
2024-02-03 13:54:31 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Program that takes in multiple numbers, and shows the average, min, max, and standard deviation.
+// Inputs: numbers
+// Process: calculate average, min, max, and standard deviation.
+// Outputs: show average, min, max, and standard deviation.
 fn main() {
     println!("Hello, world!");
 }
